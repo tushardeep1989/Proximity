f18244ec9fe827c62eb68cc4192220480c0773a5
tushardeep1989 <39212834+tushardeep1989@users.noreply.github.com>
2021-10-19 10:03:01 +0530

    Add files via upload

---
+++ disttttt.py
@@ -0,0 +1,79 @@
+
+from pymongo import MongoClient as mc
+from pymongo import errors as mongoerror
+from pymongo import *
+
+#mongo_host="sundry.wifitics.com"
+mongo_port=27017
+
+#mongo_proximity1_username="proxiana"
+#mongo_proximity1_password="proximfiltereddata"
+mongo_host="3.6.162.156"
+mongo_proximity2_username="tushar"
+mongo_proximity2_password="tushar422"
+
+
+
+
+mongo_proximity_db="proximity_macids"
+url="mongodb://{}:{}@{}:{}/?authSource={}"
+url=url.format(mongo_proximity2_username,mongo_proximity2_password,mongo_host,mongo_port,mongo_proximity_db)
+mongo_client=mc(url,appname="wt_mongo_client",connect=False,maxPoolSize=500)
+mongo_conn=mongo_client.proximity_macids
+
+
+url2="mongodb://{}:{}@{}:{}/?authSource=proximity_distance_filter&authMechanism=SCRAM-SHA-256"
+url2=url2.format(mongo_proximity2_username,mongo_proximity2_password,mongo_host,mongo_port)
+mongo_client2=mc(url2,appname="wt_mongo_client2",connect=False,maxPoolSize=500)
+db=mongo_client2['proximity_distance_filter']
+collection = db['18:A6:F7:72:D5:CA']
+#mongo_conn=mongo_client2.proximity_distance
+#mongo_conn = mongo_client2['proximity_distance_filter']
+
+#mongo_proximity1_username="proxiana"
+#mongo_proximity1_password="proximfiltereddata"
+mongo_proximity2_username="nmswifitics"
+mongo_proximity2_password="nms221308"
+
+
+
+
+a=list(mongo_conn.list_collection_names())
+
+
+prefixes = ('IMF', 'RU','ping_')
+newlist = [x for x in a if not x.startswith(prefixes)]
+print(newlist)
+
+
+cursor=mongo_conn['18:A6:F7:72:D5:CA'].find()
+
+docs=list(cursor)
+mydictionary= {}
+
+for i in range(len(docs)):
+    ans =0
+    rss = ((docs[i]['rss']))
+    callingstationid = str(((docs[i]['callingstationid'])))
+    ans = str((10 ** ((-69 - (rss)) / (10 * 2))))
+    mydictionary['callingstationid']=callingstationid
+    mydictionary['ans']=ans
+    collection.insert_one(mydictionary)
+    callingstationid.pop(0)
+    i=i+1
+
+D_0M_5M=0
+D_5M_10M=10
+for value in ans :
+    if value >=1 and value <5 :
+        D_0M_5M=D_0M_5M+1
+    elif 5 <= value < 10:
+        D_5M_10M = D_5M_10M + 1
+print(D_0M_5M)
+
+#add a dictionary for distance corresponding to calling station id's - i am working on this
+#dump the data in the table proximity_distance_filter
+# this is being done for a single router which is a table(collection) in the DB.
+# do this for all the tables -- follow the code in space 2
+
+
